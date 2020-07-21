e737ec4e2b051eff72601f5e65a8d539d00f041e
erikalvar <erikalvarbass@gmail.com>
2020-07-21 16:58:17 -0400

    adds users edit and delete

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -1,54 +1,29 @@
 <template>
-  <div class="events-edit">
-    <form v-on:submit.prevent="editEvent">
-      <h1>Edit an Event</h1>
+  <div class="users-edit">
+    <form v-on:submit.prevent="editUser">
+      <h1>Edit Profile</h1>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
       <div class="form-group">
-        <label>Title:</label>
-        <input type="text" class="form-control" v-model="event.title">
+        <label>Username:</label>
+        <input type="text" class="form-control" v-model="user.username">
       </div>
       <div class="form-group">
-        <label>Description:</label>
-        <textarea v-model="event.description" ></textarea>
+        <label>Email:</label>
+        <input type="text" class="form-control" v-model="user.email">
       </div>
       <div class="form-group">
-        <label>Venue:</label>
-        <input type="text" class="form-control" v-model="event.venue">
+        <label>Password:</label>
+        <input type="text" class="form-control" v-model="user.password">
       </div>
       <div class="form-group">
-        <label>Venue Address:</label>
-        <input type="text" class="form-control" v-model="event.venue_address">
-      </div>
-      <div class="form-group">
-        <label>Image Url:</label>
-        <input type="text" class="form-control" v-model="event.image_url">
-      </div>
-      <div class="form-group">
-        <label>Tickets Url:</label>
-        <input type="text" class="form-control" v-model="event.tickets_url">
-      </div>
-      <div class="form-group">
-        <label>Start Time:</label>
-        <datetime type="datetime" v-model="event.start_time" use12-hour></datetime>
-      </div>
-      <div class="form-group">
-        <label>End Time:</label>
-        <datetime type="datetime" v-model="event.end_time" use12-hour></datetime>
+        <label>Password Confirmation:</label>
+        <input type="text" class="form-control" v-model="user.passwordConfirmation">
       </div>
       <input type="submit" class="btn btn-primary" value="Submit" /> <br>
-      <button v-on:click="destroyEvent()">Delete Event</button>
     </form>
-
-    <br>
-    <!-- tags box -->
-    <div v-for="tag in tags">
-      <input type="checkbox" id="tag" :value="tag.id" v-model="selectedTagsIds">
-      <label for="tag">{{ tag.name }}</label>
-    </div>
-    <span>Checked tag ids: {{ selectedTagsIds }}</span>
-
+    <button v-on:click="destroyUser()">Delete User</button>
 
   </div>
 </template>
@@ -62,51 +37,39 @@ export default {
   data: function() {
     return {
       errors: [],
-      event: {},
-      tags: [],
-      selectedTagsIds: []
+      user: {}
     };
   },
   created: function() {
-    axios.get(`/api/events/${this.$route.params.id}`).then(response => {
-      this.event = response.data;
-      console.log(this.event);
-      this.selectedTagsIds = this.event.tags.map(tag => tag.id);
-    });
-    axios.get("/api/tags").then(response => {
-      this.tags = response.data;
-      console.log(this.tags);
+    axios.get(`/api/users/${this.$route.params.id}`).then(response => {
+      console.log(response.data);
+      this.user = response.data;
     });
   },
   methods: {
-    editEvent: function() {
+    editUser: function() {
       var params = {
         errors: [],
-        title: this.event.title,
-        description: this.event.description,
-        venue: this.event.venue,
-        venue_address: this.event.venue_address,
-        image_url: this.event.image_url,
-        tickets_url: this.event.tickets_url,
-        start_time: this.event.start_time,
-        end_time: this.event.end_time,
-        tag_ids: this.selectedTagsIds
+        username: this.user.username,
+        email: this.user.email,
+        password: this.user.password,
+        password_confirmation: this.user.passwordConfirmation
       };
       axios
-        .patch(`/api/events/${this.event.id}`, params)
+        .patch(`/api/users/${this.user.id}`, params)
         .then(response => {
-          this.$router.push(`/events/${response.data.id}`);
+          this.$router.push(`/users/${response.data.id}`);
         })
         .catch(error => {
           console.log(error.response.data.errors);
           this.errors = error.response.data.errors;
         });
     },
-    destroyEvent: function() {
-      if (confirm("Are you sure you want to delete event?")) {
-        axios.delete(`/api/events/${this.event.id}`).then(response => {
-          console.log("Event successfully deleted", response.data);
-          this.$router.push("/events");
+    destroyUser: function() {
+      if (confirm("Are you sure you want to delete user?")) {
+        axios.delete(`/api/users/${this.user.id}`).then(response => {
+          console.log("User successfully deleted", response.data);
+          this.$router.push("/");
         });
       }
     }
